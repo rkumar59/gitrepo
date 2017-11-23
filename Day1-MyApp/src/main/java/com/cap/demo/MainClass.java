572e6c0476e625e6be407b8096425572834a3594
lnc <lnc@in-pnq-coe11.corp.capgemini.com>
2017-11-23 16:43:07 +0530

    my first commit

---
+++ Day1-MyApp/src/main/java/com/cap/demo/MainClass.java
@@ -0,0 +1,21 @@
+package com.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		SalesClass obj=new SalesClass();
+		obj.display();
+		greeting();
+
+	}
+	
+	public static void greeting(){
+		
+	System.out.println("Hi Rajeev This is Your First GIT Application");
+	}
+
+}
---
+++ Day1-MyApp/src/main/java/com/cap/demo/SalesClass.java
@@ -0,0 +1,10 @@
+package com.cap.demo;
+
+public class SalesClass {
+	
+	public static void display(){
+		
+		System.out.println("Under SalesClass:display()");
+	}
+
+}
